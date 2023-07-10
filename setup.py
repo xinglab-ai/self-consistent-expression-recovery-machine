fb6596437a2a2a204deba8a4f5283a549e6de051
Vasudha Jha <reachvasudha27@gmail.com>
2023-07-10 12:45:39 -0700

    making changes to workflow to make it work with conda

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-if __name__ == "__main__":
-    setup()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="serm",
+    version="0.0.1",
+    author="Md Tauhidul Islam",
+    author_email="tauhid@stanford.edu",
+    description="SERM is a high-performance data-driven gene expression recovery framework.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xinglab-ai/self-consistent-expression-recovery-machine",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=open("requirements.txt").readlines(),
+    include_package_data=True,
+)
